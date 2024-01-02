33d151ffe636b72044232a9a0e01315747856893
kurtaking <kurtaking@gmail.com>
2024-01-02 15:19:37 -0700

    feat: add material and example components
    Signed-off-by: kurtaking <kurtaking@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,86 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import {
+  Container,
+  Divider,
+  Toolbar,
+  Typography,
+  AppBar,
+  Paper,
+  Card,
+  CardContent,
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+
+const pages = ["Products"];
+
+const SecondTry = () => {
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LONESTAR LAWNCARE
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <Typography>LONESTAR LAWNCARE</Typography>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))} */}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Typography>111-111-1111</Typography>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          lonestar lawncare&nbsp;
-          <br /> corsicana, tx 75110&nbsp;
-          <br />
-          <br />
-          Jaxson Brown
-          <br />
-        </p>
-      </div>
-    </main>
+    <>
+      <SecondTry />
+      <Container sx={{ mt: 12 }}>
+        <Card sx={{ mt: 5 }}>
+          <CardContent>
+            <Typography variant="h6">
+              Jaxson Brown <br />
+            </Typography>
+            <Typography variant="body2">
+              111-111-1111 <br />
+              Corsicana, Texas 75110
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
   );
 }
